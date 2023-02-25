3faa079e64fc3d86352757500b4a82805350cf23
PedroMonteiro <pedro.montolive@gmail.com>
2023-02-25 12:31:28 -0300

    add account

--- poo/exercicios/exercicio_01.php
+++ poo/exercicios/exercicio_01.php
@@ -1,19 +1,21 @@
 <?php
-
+include('../debug.php');
 class Teste {
-    public $a;
-    public $b;
+    public int $a;
+    public int $b;
 
-    public function __construct($a, $b) {
+    public function __construct(int $a, int $b) {
         $this->a = $a;
         $this->b = $b;
     }
 
-    public function soma($c) {
+    public function soma(int $c) {
         return $this->a + $this->b + $c;
     }
 }
 
-$teste = new Teste(1, 2);
+$teste = new Teste(2, 2);
 
 echo $teste->soma(3);
+
+Debug::dd($teste);
---
+++ poo/exercicios/exercicio_02.php
@@ -0,0 +1,88 @@
+<?php
+
+Class Account
+{
+    public string $bank;
+    public string $agency;
+    public string $account;
+    public float $balance;
+
+    public function __construct(string $bank, string $agency, string $account, float $balance)
+    {
+        $this->bank = $bank;
+        $this->agency = $agency;
+        $this->account = $account;
+        $this->balance = $balance;
+    }
+
+    public function deposit(float $value): void
+    {
+        $this->balance += $value;
+    }
+
+    public function withdraw(float $value): void
+    {
+        $this->balance -= $value;
+    }
+
+    public function transfer(float $value, Account $account): void
+    {
+        $this->withdraw($value);
+        $account->deposit($value);
+    }
+
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    public function getBank(): string
+    {
+        return $this->bank;
+    }
+
+    public function getAgency(): string
+    {
+        return $this->agency;
+    }
+
+    public function getAccount(): string
+    {
+        return $this->account;
+    }
+
+    public function setBank(string $bank): void
+    {
+        $this->bank = $bank;
+    }
+
+    public function setAgency(string $agency): void
+    {
+        $this->agency = $agency;
+    }
+
+    public function setAccount(string $account): void
+    {
+        $this->account = $account;
+    }
+
+    public function setBalance(float $balance): void
+    {
+        $this->balance = $balance;
+    }
+
+    public function __toString(): string
+    {
+        return "Bank: {$this->bank} Agency: {$this->agency} Account: {$this->account} Balance: {$this->balance}";
+    }
+    
+}
+
+$account_001 = new Account('Banco do Brasil', '1234', '123456-7', 1000);
+$account_002 = new Account('Caixa Econômica', '4321', '765432-1', 500);
+
+$account_001->deposit(100);
+$account_001->withdraw(50);
+$account_001->transfer(100, $account_002);
+
+echo $account_001->__toString();
